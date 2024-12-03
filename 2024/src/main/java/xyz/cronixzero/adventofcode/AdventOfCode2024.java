6dd6c0b69ccc1ae6c62e53061a539be15067603d
CronixZero <cronixfree@gmail.com>
2024-12-03 15:27:29 +0100

    feat: Add Log4J2

--- 2024/src/main/java/xyz/cronixzero/adventofcode/AdventOfCode2024.java
+++ 2024/src/main/java/xyz/cronixzero/adventofcode/AdventOfCode2024.java
@@ -1,16 +1,18 @@
 
 package xyz.cronixzero.adventofcode;
 
+import lombok.extern.log4j.Log4j2;
 import xyz.cronixzero.adventofcode.common.Day;
 import xyz.cronixzero.adventofcode.days.Day3;
 
+@Log4j2
 public class AdventOfCode2024 {
 
   public static void main(String[] args) {
     Day day = new Day3();
 
-    System.out.println(day.partOne());
-    System.out.println(day.partTwo());
+    log.info("Part 1: {}", day.partOne());
+    log.info("Part 2: {}", day.partTwo());
   }
 
 }
